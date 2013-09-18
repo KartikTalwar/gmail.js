20bbc11a25c64973da5aecfce3768aadc3a25789
Kartik Talwar <hi@kartikt.com>
2013-09-17 20:30:29 -0400

    Added check.is_preview_pane()

--- gmail.js
+++ gmail.js
@@ -3,7 +3,9 @@ var Gmail =  function() {
 	var api = {
 						  get : {},
 						  observe : {},
-						  check : {}
+						  check : {},
+						  detect : {},
+						  dom : {}
 						};
 
 
@@ -23,6 +25,7 @@ var Gmail =  function() {
 	  return user_email.replace(/['"]/g, '').trim();
 	};
 
+
 	api.check.is_thread = function() {
 	  var check_1 = $($($('.nH .if').children()[1]).children().children()[1]).children();
 	  var check_2 = $($($($('.nH .if').children()[1]).children()).children()[1]).children();
@@ -30,6 +33,19 @@ var Gmail =  function() {
 	  return check_1.length > 1 || check_2.length > 1;
 	};
 
-	return api;
 
+	api.dom.inbox_content = function() {
+		return $('div[role=main]:first');
+	}
+
+
+	api.check.is_preview_pane = function() {
+		var dom = api.dom.inbox_content();
+		var box = dom.find("[gh=tl]")[0];
+
+		return box.getAttribute('class').indexOf('aia') != -1;
+	}
+
+
+	return api;
 }
