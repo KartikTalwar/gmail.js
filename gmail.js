5fb001eab83ee5918c71c8754db0295108badd69
Kartik Talwar <hi@kartikt.com>
2013-09-17 20:05:21 -0400

    Added is_thread() check

--- gmail.js
+++ gmail.js
@@ -1,6 +1,13 @@
-var Gmail =  {
+var Gmail =  function() {
 
-	user_email : function() {
+	var api = {
+						  get : {},
+						  observe : {},
+						  check : {}
+						};
+
+
+	api.get.user_email = function() {
 		var user_email = null;
 
 	  if ($("#gbgs4dn").length == 0) {
@@ -16,5 +23,13 @@ var Gmail =  {
 	  return user_email.replace(/['"]/g, '').trim();
 	};
 
+	api.check.is_thread = function() {
+	  var check_1 = $($($('.nH .if').children()[1]).children().children()[1]).children();
+	  var check_2 = $($($($('.nH .if').children()[1]).children()).children()[1]).children();
+
+	  return check_1.length > 1 || check_2.length > 1;
+	};
+
+	return api;
 
 }
