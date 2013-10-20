3bafad0c2236ac28bf2172fed0dae8deec7099e9
Kartik Talwar <hi@kartikt.com>
2013-10-19 23:43:57 -0400

    Added api.tools.deparam

--- gmail.js
+++ gmail.js
@@ -317,6 +317,68 @@ var Gmail =  function() {
   }
 
 
+  api.tools.deparam = function (params, coerce) {
+
+    var each = function (arr, fnc) {
+      var data = [];
+      for (i = 0; i < arr.length; i++) {
+        data.push(fnc(arr[i]));
+      }
+      return data;
+    };
+
+    var isArray = Array.isArray || function(obj) {
+      return Object.prototype.toString.call(obj) == '[object Array]';
+    };
+
+    var obj = {},
+      coerce_types = {
+        'true': !0,
+        'false': !1,
+        'null': null
+      };
+    each(params.replace(/\+/g, ' ').split('&'), function (v, j) {
+      var param = v.split('='),
+        key = decodeURIComponent(param[0]),
+        val,
+        cur = obj,
+        i = 0,
+        keys = key.split(']['),
+        keys_last = keys.length - 1;
+      if (/\[/.test(keys[0]) && /\]$/.test(keys[keys_last])) {
+        keys[keys_last] = keys[keys_last].replace(/\]$/, '');
+        keys = keys.shift().split('[').concat(keys);
+        keys_last = keys.length - 1;
+      } else {
+        keys_last = 0;
+      }
+      if (param.length === 2) {
+        val = decodeURIComponent(param[1]);
+        if (coerce) {
+          val = val && !isNaN(val) ? +val : val === 'undefined' ? undefined : coerce_types[val] !== undefined ? coerce_types[val] : val;
+        }
+        if (keys_last) {
+          for (; i <= keys_last; i++) {
+            key = keys[i] === '' ? cur.length : keys[i];
+            cur = cur[key] = i < keys_last ? cur[key] || (keys[i + 1] && isNaN(keys[i + 1]) ? {} : []) : val;
+          }
+        } else {
+          if (isArray(obj[key])) {
+            obj[key].push(val);
+          } else if (obj[key] !== undefined) {
+            obj[key] = [obj[key], val];
+          } else {
+            obj[key] = val;
+          }
+        }
+      } else if (key) {
+        obj[key] = coerce ? undefined : '';
+      }
+    });
+    return obj;
+  }
+
+
   api.tools.parse_actions = function(params) {
     if(params.method == 'POST' && typeof params.url.act == 'string') {
       console.log(params.url.act, params.body);
