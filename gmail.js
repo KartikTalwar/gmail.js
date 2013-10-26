effb96a6afb505e7632ec2d6b7d17efed1ca5454
Kartik Talwar <hi@kartikt.com>
2013-10-26 02:36:30 -0400

    Added api.get.email_data()

--- gmail.js
+++ gmail.js
@@ -805,6 +805,23 @@ var Gmail =  function() {
   }
 
 
+  api.get.email_data = function() {
+    if(api.check.is_inside_email()) {
+      var url = 'https://mail.google.com/mail/u/0/?ui=2&ik=' + api.tracker.ik + '&rid=' + api.tracker.rid + '&view=cv&th=' + api.get.email_id() + '&msgs=&mb=0&rt=1&search=inbox';
+      var get_data = api.tools.make_request(url);
+          get_data = get_data.substring(get_data.indexOf('['), get_data.length);
+          get_data = 'var cdata = ' + get_data;
+
+      eval(get_data);
+
+      api.tracker.email_data = cdata[0];
+
+      return api.tools.parse_email_data(api.tracker.email_data);
+    }
+
+    return {};
+  }
+
+
   return api;
 }
-a.tools.make_request('https://mail.google.com/mail/u/0/?ui=2&ik=13fa7f7088&rid=mail%3Aex.48d0.23.0&view=cv&th=141de25dc0b48e4f&msgs=&_reqid=2104851&pcd=1&mb=0&rt=1&search=starred')
