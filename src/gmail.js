e2cfe39690e5d635d0f65b3cdfd14010f496bb1a
Xavi <xavi.rmz@gmail.com>
2013-12-01 15:12:06 -0800

    Removed redundant check.

--- src/gmail.js
+++ src/gmail.js
@@ -46,10 +46,9 @@ var Gmail =  function() {
 
   api.check.is_thread = function() {
     var check_1 = $('.nH .if').children(":eq(1)").children().children(":eq(1)").children();
-    var check_2 = $('.nH .if').children(":eq(1)").children().children(":eq(1)").children();
-    var check_3 = api.get.email_ids();
+    var check_2 = api.get.email_ids();
 
-    return (check_1.length > 1 || check_2.length > 1) || check_3.length > 1;
+    return check_1.length > 1 || check_2.length > 1;
   };
 
 
