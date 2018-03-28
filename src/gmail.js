8870f0c01bab26bff52ceee81cbcd2bb737d31ef
Jostein Kjønigsen <jostein@kjonigsen.net>
2018-03-28 14:42:16 +0200

    Tentative fix for XHR initialization with no js_frame present.

--- src/gmail.js
+++ src/gmail.js
@@ -1062,15 +1062,25 @@ var Gmail_ = function(localJQuery) {
         if (!api.tracker.xhr_init) {
             api.tracker.xhr_init = true;
             var js_frame = null;
+
             if (top.document.getElementById("js_frame")){
                 js_frame = top.document.getElementById("js_frame");
             } else if (window.opener) {
                 js_frame = window.opener.top.document.getElementById("js_frame");
             }
             if (!js_frame){
-                throw "Cannot register the xhr watcher as mail.google.com is not fully loaded yet. Please wrap your code in `gmail.observe.on(\"load\")`";
+                if (window.opener) {
+                    js_frame = window.opener.top;
+                } else {
+                    js_frame = top;
+                }
             }
-            var win = js_frame.contentDocument.defaultView;
+            var win;
+            if (js_frame.contentDocument) {
+                win = js_frame.contentDocument.defaultView;
+            } else {
+                win = js_frame;
+            }
 
             if (!win.gjs_XMLHttpRequest_open) {
                 win.gjs_XMLHttpRequest_open = win.XMLHttpRequest.prototype.open;
