caab2e96b0cd34f518f61b2a9dc76071d1ce41d7
Artsiom Menshakou <menshakov.artem@gmail.com>
2018-06-18 10:53:45 +0300

    fix tests

--- test/test.tools.js
+++ test/test.tools.js
@@ -60,6 +60,31 @@ describe("Monkeypatching", () => {
 
 describe("Test tools for parsing new gmail body_params", () => {
     const gmail = new Gmail();
+    const data =  JSON.parse(testData.new_gmail_archive_action_body_params);
+    const threads = gmail.tools.extract_from_graph(data, gmail.check.data.is_thread);
+    const threadData = threads.map(thread => gmail.tools.get_thread_data(thread))[0];
+
+    it("get thread id", () => {
+        const thread = gmail.tools.get_thread_id(threads[0]);
+
+        assert.equal(thread, 'thread-f:1603171109786600032');
+    });
+
+    it("get thread data", () => {
+        const mockThreadData = threads[0][2][7];
+        const threadData = gmail.tools.get_thread_data(threads[0]);
+        console.log(threadData);
+
+        assert.deepEqual(threadData, mockThreadData);
+    });
+
+    it("get messages ids", () => {
+        const mockMessageIds = ['msg-f:1603171109786600032', 'msg-f:1603245801543734539', 'msg-f:1603245862071354412', 'msg-f:1603246018478443087', 'msg-f:1603256094012730022', 'msg-f:1603256564311088576', 'msg-f:1603256665279246114', 'msg-f:1603256682384715664', 'msg-f:1603376719891477511', 'msg-f:1603376909485932601', 'msg-f:1603376994923202634', 'msg-f:1603380395240179639'];
+        const messagesIds = gmail.tools.get_message_ids(threadData);
+
+        assert.equal(messagesIds.length, 12);
+        assert.deepEqual(messagesIds, mockMessageIds);
+    });
 
     it("parses archived messages", () => {
         const xhrData = testData.new_gmail_archive_action_body_params;
