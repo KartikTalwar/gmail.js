f4b5f42a2cbf6bc1c81b6571fc5b35f055799a23
shashikiran-im <shashi.kiran@imanage.com>
2019-11-08 15:55:50 +0530

    Fix issues with "label" event on new Gmail
    - Add test case
    
    This closes Issue #493.

--- src/gmail.js
+++ src/gmail.js
@@ -1116,6 +1116,7 @@ var Gmail = function(localJQuery) {
     };
 
     api.tools.check_event_type = function(threadObj) {
+        const apply_label = "^x_";
         const action_map = {
             // ""            : "add_to_tasks",
             "^a": "archive",
@@ -1126,7 +1127,7 @@ var Gmail = function(localJQuery) {
             // ""            : "discard_draft",
             // ""            : "expand_categories",
             // ""            : "filter_messages_like_these",
-            // ""            : "label",
+            "^x_"            : "label",
             // "^io_im^imi": "mark_as_important",
             // "^imn": "mark_as_not_important",
             // ""            : "mark_as_not_spam",
@@ -1154,8 +1155,14 @@ var Gmail = function(localJQuery) {
 
         if (threadData && api.check.data.is_action(threadData)) {
             const action = api.tools.get_action(threadData);
-
-            return action_map[action];
+            
+            //Check if label is applied to email / existing email is moved to an label
+            if(action.startsWith(apply_label) && api.check.data.is_first_type_action(threadData)) {
+                return action_map[apply_label];
+            } else {
+                return action_map[action];
+            }
+            
         } else {
             return null;
         }
