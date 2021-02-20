fce0ae72e3aaf23cfa96ce1e02a1d9a35be39706
ibercovich <ibercovich@gmail.com>
2021-02-20 01:03:06 -0800

    I want to be able to get the trimmed part of the body when editing - … (#639)

--- src/gmail.js
+++ src/gmail.js
@@ -3905,6 +3905,7 @@ var Gmail = function(localJQuery) {
                 subjectbox: "input[name=subjectbox]",
                 all_subjects: "input[name=subjectbox], input[name=subject]",
                 body: "div[contenteditable=true]",
+                quoted_reply: "input[name=uet]",
                 reply: "M9",
                 forward: "M9",
                 from: "input[name=from]",
